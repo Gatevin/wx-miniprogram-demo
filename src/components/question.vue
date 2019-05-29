44d2f4c77b4613fc332e45ea7db1a9fbd24eae08
ichimei <ichimei@ichimeideMacBook-Pro.local>
2019-05-29 20:53:59 +0800

    initial

---
+++ src/components/question.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="question">
+      <p>{{question_description}}</p>
+      
+        <div v-for="(content, choice_id) in question_choices" v-bind:key="choice_id">
+          <input type="radio" :v-model="unique_select" :value=content> {{content}}
+        </div>
+        
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    'question_description': {
+      type: String,
+      default: 'question_description'
+    },
+    'question_choices': {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style>
+.question {
+  padding: 10px;
+}
+</style>
+<!--
+  <input type="checkbox" id="A" value="A" v-model="checkedNames">
+        <label for="A">{{question_selection}}{{A}}</label>
+        <input type="checkbox" id="B" value="B" v-model="checkedNames">
+        <label for="B">{{question_selection}}{{B}}</label>
+        <input type="checkbox" id="C" value="C" v-model="checkedNames">
+        <label for="C">{{question_selection}}{{C}}</label>
+        <input type="checkbox" id="D" value="D" v-model="checkedNames">
+        <label for="D">{{question_selection}}{{D}}</label>
+        <br>
+        <span>选择的值为: {{ checkedNames }}</span>
+  -->
---
+++ src/pages/question_page/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <question :question_description="description" :question_choices="selection"></question>
+        
+    </div>
+</template>
+
+<script>
+import question from '@/components/question'
+
+export default {
+  data () {
+    return {
+      question_id: 0,
+      description: '选出你喜欢的搜索引擎吧～',
+      selection: {
+        A: '谷歌',
+        B: '百度',
+        C: '必应',
+        D: '雅虎'
+      },
+      answer: []
+    }
+  },
+
+  components: {
+    question
+  }
+}
+</script>
